cf42b83c3c78e0800715114104b17bd049020706
cb1-61803 <76478260+cb1-61803@users.noreply.github.com>
2021-01-31 19:05:11 +0000

    first commit

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 import seaborn as sns
+
 
 df = pd.read_csv('don_sector_10year.csv')
 for column_name in df.columns:
@@ -29,6 +31,10 @@ print(df_don_sec_val_year.head())
 df_ire = df_don_sec_val_year.loc[(df_don_sec_val_year["Donor"] == 'Ireland')]
 df_ire_1 = df_ire.loc[(df_ire["Amount type"] == 'Constant Prices')]
 print(df_ire_1.head())
+
+
+
+
 df_ire_sec = df_ire_1[[ "Sector", 'Value', 'Year']]
 print("df_ire_sec = Clean Data Set")
 print(df_ire_sec.values)
@@ -49,23 +55,41 @@ df_gov = df_ire_sec.loc[(df_ire_sec ["Sector"] == 'I.5. Government & Civil Socie
 df_infra = df_ire_sec.loc[(df_ire_sec ["Sector"] == 'I.6. Other Social Infrastructure & Services')]
 
 
+outer_df =pd.merge(df_ed, df_hea, on='Year', how='outer')
+print(outer_df.head)
+labels = ['G1', 'G2', 'G3', 'G4', 'G5']
+men_means = [20, 34, 30, 35, 27]
+women_means = [25, 32, 34, 20, 25]
+
+x = np.arange(len(labels))  # the label locations
+width = 0.35  # the width of the bars
 
 fig, ax = plt.subplots()
-ax.plot(df_ed["Year"], df_ed["Value"])
-# Customize the x-axis label
-ax.set_xlabel("Year")
-# Customize the y-axis label
-ax.set_ylabel("USD (million)")
-# Add the title
-ax.set_title("Ireland's ODA contribution to Education")
-plt.show()
+rects1 = ax.bar(x - width/2, men_means, width, label='Men')
+rects2 = ax.bar(x + width/2, women_means, width, label='Women')
 
-fig, ax = plt.subplots()
-ax.plot(df_hea["Year"], df_hea["Value"])
-# Customize the x-axis label
-ax.set_xlabel("Year")
-# Customize the y-axis label
-ax.set_ylabel("USD (million)")
-# Add the title
-ax.set_title("Ireland's ODA contribution to Health")
+# Add some text for labels, title and custom x-axis tick labels, etc.
+ax.set_ylabel('Scores')
+ax.set_title('Scores by group and gender')
+ax.set_xticks(x)
+ax.set_xticklabels(labels)
+ax.legend()
+
+
+def autolabel(rects):
+    """Attach a text label above each bar in *rects*, displaying its height."""
+    for rect in rects:
+        height = rect.get_height()
+        ax.annotate('{}'.format(height),
+                    xy=(rect.get_x() + rect.get_width() / 2, height),
+                    xytext=(0, 3),  # 3 points vertical offset
+                    textcoords="offset points",
+                    ha='center', va='bottom')
+
+
+autolabel(rects1)
+autolabel(rects2)
+
+fig.tight_layout()
+
 plt.show()
