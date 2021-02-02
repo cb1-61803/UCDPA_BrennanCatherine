78025223440565e4e0d15df017b5d997970a3193
cb1-61803 <76478260+cb1-61803@users.noreply.github.com>
2021-02-02 16:37:58 +0000

    tidy

--- main.py
+++ main.py
@@ -22,38 +22,61 @@ print(df.columns)
 # Print the row index of df
 print(df.index)
 # Sort by Aid type
-df_at = df.sort_values('Aid type')
-print(df_at.head())
-df_don = df.sort_values('Donor')
-print(df_don.head())
+print(df.sort_values('Aid type'))
+print(df.sort_values('Donor'))
+#df_ire_1 = df_ire_at.drop_duplicates(subset ="Sector")
 df_don_sec_val_year = df[['Donor', "Sector", 'Value', 'Year','Amount type']]
 print(df_don_sec_val_year.head())
-#Ireland DONOR = 21
+
+
 df_ire = df_don_sec_val_year.loc[(df_don_sec_val_year["Donor"] == 'Ireland')]
 df_ire_1 = df_ire.loc[(df_ire["Amount type"] == 'Constant Prices')]
 print(df_ire_1.head())
-
-
-
-
 df_ire_sec = df_ire_1[[ "Sector", 'Value', 'Year']]
+
 print("df_ire_sec = Clean Data Set")
+print(df_ire_sec.head())
 print(df_ire_sec.values)
 print(df_ire_sec.shape)
+
 print("df_ire_ed = Test for Education")
 df_ire_ed = df_ire_sec.loc[(df_ire_sec["Sector"] == 'I.1. Education')]
-
 print(df_ire_ed.head())
 print(df_ire_ed.shape)
+
 df_ed = df_ire_ed[['Value', 'Year']]
 print(df_ed.head())
 print(df_ed.shape)
-#print('function for recreating DFs')
+
+
 df_hea = df_ire_sec.loc[(df_ire_sec ["Sector"] == 'I.2. Health')]
 df_pop = df_ire_sec.loc[(df_ire_sec ["Sector"] == 'I.3. Population Policies/Programmes & Reproductive Health')]
 df_watsan = df_ire_sec.loc[(df_ire_sec ["Sector"] == 'I.4. Water Supply & Sanitation')]
 df_gov = df_ire_sec.loc[(df_ire_sec ["Sector"] == 'I.5. Government & Civil Society')]
 df_infra = df_ire_sec.loc[(df_ire_sec ["Sector"] == 'I.6. Other Social Infrastructure & Services')]
+
+
+#clean df df_ire_sec sector value year
+
+# Define a function
+def sector(axes, x, y, color, xlabel, ylabel):
+  axes.plot(x, y, color=color)
+  axes.set_xlabel(xlabel)
+  axes.set_ylabel(ylabel, color=color)
+  axes.tick_params('y', colors=color)
+
+
+fig, ax = plt.subplots()
+sector(ax, df_ed ["Year"], df_ed['Value'], 'blue', "Year", "USD millions")
+ax2 = ax.twinx()
+sector(ax2, df_pop["Year"], df_pop['Value'], 'red', "Year", "USD millions")
+ax.set_title("Education and Population/Reproductive Health")
+plt.show()
+
+
+
+
+
 
 # Create a Figure and an Axes with plt.subplots
 fig, ax = plt.subplots()
@@ -65,72 +88,37 @@ ax.plot(df_pop["Year"], df_pop["Value"], color='r', marker='o', linestyle='--')
 ax.plot(df_watsan["Year"], df_watsan["Value"], color='c', marker='o', linestyle='--')
 ax.plot(df_gov["Year"], df_gov["Value"], color='y', marker='o', linestyle='--')
 ax.plot(df_infra["Year"], df_infra["Value"], color='m', marker='o', linestyle='--')
-# Customize the x-axis label
 ax.set_xlabel("Year")
-
-# Customize the y-axis label
 ax.set_ylabel("US Dollar, Millions, 2018")
-
-# Add the title
 ax.set_title("Ireland's ODA to Social Infrastructure & Services")
-
 ed = mpatches.Patch(color= 'b', label='Education')
 hea = mpatches.Patch(color= 'g', label='Health')
 pop = mpatches.Patch(color= 'r', label='Population')
 watsan = mpatches.Patch(color= 'c', label='Water')
 gov = mpatches.Patch(color= 'y', label='Governance')
 infra = mpatches.Patch(color= 'm', label='Infrastructure')
-
-
 plt.legend(handles=[ed, hea, pop, watsan, gov,infra], prop={'size': 6})
+
 #plt.legend([df_ed, df_hea, df_pop, df_watsan, df_gov, df_infra],["Education", "Health", "Population", "Water", "Governance", "infrastructure"])
 #ax.legend()
-# Call the show function
+
 plt.show()
 
 #fig, ax = plt.subplots(3, 2)
 
 # Create a figure and an array of axes: 2 rows, 1 column with shared y axis
-#fig, ax = plt.subplots(3, 2, sharey=True)
+fig, ax = plt.subplots(3, 2, sharey=True)
 
 
-#ax[0].plot(df_ed["Year"], df_ed["Value"], color='b', marker='o', linestyle='--')
-#ax.plot(df_hea["Year"], df_hea["Value"], color='g', marker='o', linestyle='--')
-#ax.plot(df_pop["Year"], df_pop["Value"], color='r', marker='o', linestyle='--')
-#ax.plot(df_watsan["Year"], df_watsan["Value"], color='c', marker='o', linestyle='--')
-#ax.plot(df_gov["Year"], df_gov["Value"], color='y', marker='o', linestyle='--')
-#ax.plot(df_infra["Year"], df_infra["Value"], color='m', marker='o', linestyle='--')
+ax[0].plot(df_ed["Year"], df_ed["Value"], color='b', marker='o', linestyle='--')
+ax[1].plot(df_hea["Year"], df_hea["Value"], color='g', marker='o', linestyle='--')
+ax[2].plot(df_pop["Year"], df_pop["Value"], color='r', marker='o', linestyle='--')
+ax[3].plot(df_watsan["Year"], df_watsan["Value"], color='c', marker='o', linestyle='--')
+ax[4].plot(df_gov["Year"], df_gov["Value"], color='y', marker='o', linestyle='--')
+ax[5].plot(df_infra["Year"], df_infra["Value"], color='m', marker='o', linestyle='--')
 
 
 
-#clean df df_ire_sec sector value year
-# Define a function called plot_timeseries
-def sector(axes, x, y, color, xlabel, ylabel):
-  # Plot the inputs x,y in the provided color
-  axes.plot(x, y, color=color)
-  # Set the x-axis label
-  axes.set_xlabel(xlabel)
-  # Set the y-axis label
-  axes.set_ylabel(ylabel, color=color)
-  # Set the colors tick params for y-axis
-  axes.tick_params('y', colors=color)
-
-
-fig, ax = plt.subplots()
-
-
-# Plot the CO2 levels time-series in blue
-sector(ax, df_ed ["Year"], df_ed['Value'], 'blue', "Year", "USD millions")
-
-# Create a twin Axes object that shares the x-axis
-ax2 = ax.twinx()
-
-# Plot the relative temperature data in red
-sector(ax2, df_pop["Year"], df_pop['Value'], 'red', "Year", "USD millions")
-
-ax.set_title("Education and Population/Reproductive Health")
-
-plt.show()
 
 
 
