a76d8ca2ecf0258d1b0bcf37a51cde5f14a5c3bb
cb1-61803 <76478260+cb1-61803@users.noreply.github.com>
2021-08-15 17:12:45 +0100

    7th  commit - gen_env dataset

--- main.py
+++ main.py
@@ -89,7 +89,7 @@ print(df.head())
 #res_string = re,sub(r"", replace with, target_string)
 
 df16 = pd.read_csv('purpose_16.csv')
-df16['purpose'] = df16['purpose'].astype('int64')
+df16 = df16.astype('float64')
 print(df16.head())
 index16 = df16.index
 print(len(index16))
@@ -99,28 +99,28 @@ print(df16.dtypes)
 #df161 = df16.replace(to_replace='\w*', value= NaN, regex=True)
 
 df17 = pd.read_csv('purpose_17.csv')
-df17['purpose'] = df17['purpose'].astype('int64')
+df17['purpose'] = df17.astype('float64')
 print(df17.head())
 index17 = df17.index
 print(len(index17))
 print(df17.dtypes)
 
 df18 = pd.read_csv('purpose_18.csv')
-df18['purpose'] = df18['purpose'].astype('int64')
+df18['purpose'] = df18.astype('int64')
 print(df18.head())
 index18 = df18.index
 print(len(index18))
 print(df18.dtypes)
 
 df19 = pd.read_csv('purpose_19.csv')
-df19['purpose'] = df19['purpose'].astype('int64')
+df19['purpose'] = df19.astype('float64')
 print(df19.head())
 index19 = df19.index
 print(len(index19))
 print(df19.dtypes)
 
 df20 = pd.read_csv('purpose_20.csv')
-df20['purpose'] = df20['purpose'].astype('int64')
+df20['purpose'] = df20.astype('float64')
 print('\n###DF20 metadata:')
 print(df20.head())
 index20 = df20.index
@@ -131,26 +131,27 @@ print(datatype_20)
 print(datatype_20)
 
 df_code_list = pd.read_csv('code_list.csv')
-df_code_list['purpose'] = df_code_list['purpose'].astype('int64')
-datatype_df_code_list = df_code_list.dtypes
+df_code_list['purpose'] = df_code_list['purpose'].astype('float64')
 print('\n###Code List Head:')
 print(df_code_list.head())
+datatype_df_code_list = df_code_list.dtypes
 print('\n###Code List Datatype:')
+print(df_code_list.dtypes)
 print(datatype_df_code_list)
 
-###merge data with full code list###
+###merge longest data with full code list###
 df_code_list_20 = pd.merge(df_code_list, df20, how = 'left', on ="purpose")
 print('\n### Merge 1 head:')
 print(df_code_list_20.head())
 for column_name in df_code_list_20.columns:
     print(column_name)
 
-index_df_code_list_20 = df_code_list_20.index
-print(index_df_code_list_20)
+#print(df_code_list_20.index())
 
-df_code_list_1920 = pd.merge(df_code_list_20, df19, how = 'left', on ="purpose")
+#df_code_list_1920 = pd.merge(df_code_list_20, df19, how = 'left', on ="purpose")
 
-dataframes_16to20 = [df_code_list_1920,df18, df17, df16]
+dataframes_16to20 = [df_code_list_20, df19, df18, df17, df16]
+
 #df_16to20 = reduce(lamda, left, right : pd.merg(left, right, on = ['purpose'], how = 'outer'), dataframes_16to20)
 df_16to20 = reduce(lambda left,right: pd.merge(left,right,on='purpose', how='left'), dataframes_16to20)
 print('\n### Merge 16to20')
@@ -158,30 +159,58 @@ print(df_16to20.head())
 for column_name in df_16to20.columns:
     print(column_name)
 
+print('\n### base dataset')
+df_16to20 = df_16to20.drop(columns =['DESCRIPTION', 'Clarifications / Additional notes on coverage']).copy()
+df_16to20['purpose'] = df_code_list['purpose'].astype('str')
+
+print(df_16to20.dtypes)
+
+#df_16to20_draft2 = [['purpose', '2016', '2017' , '2018', '2019', '2020', 'DESCRIPTION']]
+
 print('\n### number of rows with missing data')
-print(df_16to20.isnull().any(axis = 1).sum())
+print(df_16to20.isnull().any().sum())
 ###drop empty rows
+
 df_16to20_minus_empty_rows = df_16to20.dropna(how='any', subset = ['2020','2019','2018','2017','2016'])
 print(df_16to20_minus_empty_rows.head())
 print('\n### new number of empty rows')
-print(df_16to20_minus_empty_rows.isnull().any(axis = 1).sum())
+print(df_16to20_minus_empty_rows.isnull().any(axis = 0).sum())
 
-print('\n### view empties')
-print(df_16to20_minus_empty_rows[df_16to20_minus_empty_rows.isnull().any(axis=1)])
-print(df_16to20_minus_empty_rows.iloc[:8, :8])
-df_16to20_draft1 = df_16to20_minus_empty_rows.drop('Clarifications / Additional notes on coverage', axis = 1)
-df_16to20_draft2 = [['purpose', '2016', '2017' , '2018', '2019', '2020', 'DESCRIPTION']]
+df_16to20_amounts = df_16to20_minus_empty_rows.copy()
 
 
-print(df_16to20_minus_empty_rows.iloc[0:8, 3:])
-print(df_16to20_minus_empty_rows.describe())
-#print(df_16to20_minus_empty_rows.groupby('purpose').size())
-#turn purpose back to string
-cut the data set by cutting back purpose
-transpose?
+df_16to20_amounts['sector'] = df_16to20_amounts['purpose'].str[:2]
+print(df_16to20_amounts)
+for column_name in df_16to20_amounts.columns:
+    print(column_name)
+
+print(df_16to20_amounts.dtypes)
+#df.plot(x='purpose', y='2020', kind = 'scatter')
 
 
-#con
+df_gen_env = pd.read_csv('Gen_Env 15_20_DATA.csv')
+print(df_gen_env)
+for column_name in df_gen_env.columns:
+    print(column_name)
+print(df_gen_env.dtypes)
+
+df_gen_env['year'] = df_gen_env['year'].astype(str)
+df_gen_env['channel'] = df_gen_env['channel'].astype(str)
+df_gen_env['purposecode'] = df_gen_env['purposecode'].astype(str)
+df_gen_env['Sector'] = df_gen_env['Sector'].astype(str)
+df_gen_env['gender'] = df_gen_env['gender'].astype('bool')
+df_gen_env['environment'] = df_gen_env['environment'].astype('bool')
+df_gen_env['extended'] = df_gen_env['extended'].astype(float)
+df_gen_env_cl = df_gen_env.dropna( axis = 1)
+df_gen_env_clean = df_gen_env_cl.dropna(how='all')
+print('\n###df_gen_env_clean')
+print(df_gen_env_clean.dtypes)
+print(df_gen_env_clean.shape)
+print(df_gen_env_clean.head)
+
+
+
+
 
 
 
